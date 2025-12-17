a0a379f947fe1320bfde003cbca4dd30747162fc
Juan Bances <jcbances6@gmail.com>
2025-12-16 23:53:54 -0500

    agregando sanctum y protegiendo rutas

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('categories', CategoryController::class);
+
+    Route::post('login', [AuthController::class, 'login']);
 
     Route::get('products/find-by-name', [ProductController::class, 'findByName']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        // Route::get('user', function (Request $request) {
+            //     return $request->user();
+            // });
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::apiResource('categories', CategoryController::class);
+        Route::apiResource('products', ProductController::class);
+    });
 
-    Route::apiResource('products', ProductController::class);
+
+
+
+
+
 
 });
